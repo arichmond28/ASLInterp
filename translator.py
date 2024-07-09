3df858c3a6403cdb74ddeadf34bb0a6120cffca7
Andrew Richmond <ar744@cornell.edu>
2024-07-08 22:26:03 -0500

    Added basic image similarity example using CLIP from HuggingFace

---
+++ translator.py
@@ -0,0 +1,22 @@
+from sentence_transformers import SentenceTransformer, util
+from PIL import Image
+
+def main():
+  #Load CLIP model
+  model = SentenceTransformer('clip-ViT-L-14')
+
+  #Encode an image:
+  img_emb = model.encode(Image.open('cookie1.jpg'))
+
+  #Encode text descriptions
+  text_emb = model.encode(Image.open('cookie1again.jpg'))
+
+  img_emb2 = model.encode(Image.open('cookie2.jpeg'))
+
+  #Compute cosine similarities 
+  cos_scores = util.cos_sim(img_emb, text_emb)
+  cos_scores2 = util.cos_sim(img_emb, img_emb2)
+  print(cos_scores)
+  print(cos_scores2)
+
+main()
